b966db91296737d6574f2b091e1a38ccbebe37b5
Howard Chen <pohaoche@usc.edu>
2020-07-07 15:20:37 -0700

    <feat> Add particles Animation
    * Install package react-particles-js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
+import Particles from "react-particles-js";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5,
+      },
+    },
+    
+  },
+};
+
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
