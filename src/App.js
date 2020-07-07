54ba319f66ed5dcdca08d205d6f6be6c5240c582
Howard Chen <pohaoche@usc.edu>
2020-07-07 14:15:25 -0700

    <feat> Add basic Navigation
    * Use tachyons CND for styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      {/* <Logo />
+      <ImageLinkForm />
+      <FaceRecognition /> */}
     </div>
   );
 }
