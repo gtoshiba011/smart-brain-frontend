64fe6f7958b74a4fcda7f1e67094f850a5f8da1f
Howard Chen <pohaoche@usc.edu>
2020-07-07 23:53:19 -0700

    <feat> Apply face detection API
    * Install Clarifai
    * API key is 923bdb65ba984677ac69f43368456e4f

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
@@ -15,21 +16,48 @@ const particlesOptions = {
         blur: 5,
       },
     },
-    
   },
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles" params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: "923bdb65ba984677ac69f43368456e4f",
+});
+
+class App extends Component {
+  state = {
+    input: "",
+  };
+
+  inputChangeHandler = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  buttonSubmitHandler = () => {
+    app.models
+      .predict(
+        "a403429f2ddf4b49b307e318f00e528b",
+        "https://www.thestatesman.com/wp-content/uploads/2017/08/1493458748-beauty-face-517.jpg"
+      )
+      .then((res) => console.log(res.outputs))
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          input={this.state.input}
+          onInputChange={this.inputChangeHandler}
+          onButtonSubmit={this.buttonSubmitHandler}
+        />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
