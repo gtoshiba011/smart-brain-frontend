f6dd69e7c37888c083a12123b2d471b0a9f1d2a8
Howard Chen <pohaoche@usc.edu>
2020-07-20 03:51:08 -0700

    <feat> Implement ProfileIcon dropdown by reactstrap
    * Install reactstrap@5.0.0
    * Use bootstrap@4.0.0 and tachyons@4.9.0 instead of CDN; bug when using
    bootstrap CDN
    * import index.css later
    * Remove CND in index.html

--- src/App.js
+++ src/App.js
@@ -42,8 +42,8 @@ class App extends Component {
     input: "",
     imageUrl: "",
     boxs: [],
-    route: "signin",
-    isSignedIn: false,
+    route: "home",
+    isSignedIn: true,
     user: {
       id: "",
       name: "",
