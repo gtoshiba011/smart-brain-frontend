f14ef38df7fb472ad23aec47ba09d702f9b590af
Howard Chen <pohaoche@usc.edu>
2020-09-12 23:17:05 -0700

    <feat> apply serverless and lambda to show rank emoji

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -1,12 +1,46 @@
 import React from "react";
 
-const Rank = (props) => {
-  const { name, entries } = props;
-  return (
-    <div>
-      <div className="white f3">{`${name}, your current rank is ${entries}.`}</div>
-    </div>
-  );
-};
+class Rank extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emoji: "",
+    };
+  }
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.entries === this.props.entries &&
+      prevProps.name === this.props.name
+    ) {
+      return null;
+    }
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = (entries) => {
+    fetch(
+      `https://a18zggno4a.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`
+    )
+      .then((res) => res.json())
+      .then((data) => this.setState({ emoji: data.input }))
+      .catch(console.log);
+  };
+
+  render() {
+    const { name, entries } = this.props;
+    return (
+      <div>
+        <div className="white f3">{`${name}, your current rank is...`}</div>
+        <div className="white f1">{entries}</div>
+        <div className="white f3">{`Rank Badge: ${this.state.emoji}`}</div>
+      </div>
+    );
+  }
+}
 
 export default Rank;
